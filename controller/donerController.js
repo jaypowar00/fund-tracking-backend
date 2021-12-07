09f37b0a7b5550a3a0102167b55d3af517181411
jaypowar00 <jaypowar00@gmail.com>
2021-12-07 14:04:25 +0530

    Added login, logout, profile functionality (using jwt) for Doner

--- controller/donerController.js
+++ controller/donerController.js
@@ -3,6 +3,47 @@ const router = express.Router();
 const cors = require('cors');
 const db = require('../queries');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+var blackListedTokens = []
+
+function generateDonerAccessToken(doner_id) {
+    let access_token = jwt.sign({doner_id: doner_id}, process.env.FTSECRET_KEY, { expiresIn: '3d' });
+    console.log('accessToken: '+ access_token);
+    if (blackListedTokens.includes(access_token))
+        blackListedTokens.pop(blackListedTokens.indexOf(access_token));
+    return access_token;
+}
+
+function authenticateDoner(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (token == null)
+        return res.json({
+            status: false,
+            message: 'access token is missing in request'
+        });
+        jwt.verify(token, process.env.FTSECRET_KEY, (err, user) => {
+            if(err) {
+                console.log('err:\n');
+                console.log(err.name);
+                if (blackListedTokens.includes(token))
+                    blackListedTokens.pop(blackListedTokens.indexOf(token));
+                return res.json({
+                    status: false,
+                    message: err.message
+                });
+            }
+            if (blackListedTokens.includes(token))
+                return res.json({
+                    status: false,
+                    message: 'you have been logged out, please login again'
+                });
+        req.user = user.doner_id
+        next()
+    });
+}
 
 router.post('/register', cors(), (req, res) => {
     const {body} = req;
@@ -93,6 +134,85 @@ router.post('/register', cors(), (req, res) => {
     });
 });
 
+router.post('/login', cors(), (req, res) => {
+    const { body } = req;
+    const {
+        password
+    } = body;
+    let { email } = body;
+    if (!password) 
+        return res.json({
+            status: false,
+            message: 'Error: please provide the password'
+        });
+    if (!email) 
+        return res.json({
+            status: false,
+            message: 'Error: please provide the email'
+        });
+    email = email.toLowerCase();
+    
+    db.query("SELECT doner_id, password FROM Doners WHERE email = '"+email+"';", (error, results) => {
+        if(error)
+            return res.json({
+                status: false,
+                message: 'Error: Something went wrong while logging in the user... ('+error.message+')'
+            });
+        if (results.rows.length == 0)
+            return res.json({
+                status: false,
+                message: 'User does not exists!',
+                user_id: null
+            });
+        console.log("log::");
+        console.log(results.rows);
+        console.log(email);
+        if (bcrypt.compareSync(password, results.rows[0]['password'])) {
+            return res.json({
+                status: true,
+                message: 'Login successful',
+                user_id: results.rows[0]['doner_id'],
+                access_token: generateDonerAccessToken(results.rows[0]['doner_id'])
+            });
+        }else {
+            return res.json({
+                status: false,
+                message: 'Password Failed',
+                user_id: null
+            });
+        }
+    });
+});
+
+router.get('/profile', cors(), authenticateDoner, (req, res) => {
+    db.query('SELECT name, email, username FROM Doners WHERE doner_id='+req.user+';', (error, result) => {
+        if (error)
+            return res.json({
+                status: false,
+                message: 'Error: something went wrong ('+error.message+')'
+            });
+        return res.json({
+            status: true,
+            message: 'doner profile',
+            doner_id: req.user,
+            name: result.rows[0]['name'],
+            username: result.rows[0]['username'],
+            email: result.rows[0]['email'],
+        });
+    });
+});
+
+router.post('/logout', cors(), authenticateDoner, (req, res) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (!blackListedTokens.includes(token))
+        blackListedTokens.push(token)
+    return res.json({
+        status: true,
+        message: 'Successfully logged out'
+    });
+});
+
 router.get('/', cors(), (request, response) => {
     db.query('SELECT * FROM Doners;', (error, results) => {
         if(error) {
@@ -105,11 +225,9 @@ router.get('/', cors(), (request, response) => {
             });
         }else {
             console.log('[+] success');
-            console.log(results.rows);
-            console.log(bcrypt.hashSync("pass123", bcrypt.genSaltSync(8), null));
             return response.json({
                 status: true,
-                doners:results.rows,
+                doners: results.rows,
                 message: null,
             });
         }
